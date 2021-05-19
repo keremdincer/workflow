2cc3ebc4857a51ca9ca7f8918fcb11cb5f796eea
Kerem Dinçer <b.keremdincer@gmail.com>
2021-05-19 17:52:10 +0300

    Added new modules. - Project - Corporation - Item - Shipping

--- src/application.ts
+++ src/application.ts
@@ -1,14 +1,22 @@
-import { createApplication } from "graphql-modules";
-import { userModule } from "./modules/user/user.module";
+import { createApplication } from 'graphql-modules'
+import { SchemaDirectiveVisitor } from 'graphql-tools'
+import { schemaDirectives } from './directives'
+import { userModule } from './modules/user/user.module'
 import { authenticationModule } from './modules/authentication/authentication.module'
-import { SchemaDirectiveVisitor } from "graphql-tools";
-import { schemaDirectives } from "./directives";
+import { projectModule } from './modules/project/project.module'
+import { corporationModule } from './modules/corporation/corporation.module'
+import { itemModule } from './modules/item/item.module'
+import { shippingModule } from './modules/shipping/shipping.module'
 
 const application = createApplication({
   modules: [
     userModule,
-    authenticationModule
-  ]
+    authenticationModule,
+    projectModule,
+    corporationModule,
+    itemModule,
+    shippingModule,
+  ],
 })
 
 export const schema = application.createSchemaForApollo()
